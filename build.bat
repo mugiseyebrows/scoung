540aec536ecd40788fab0976deb570217b0dc2dd
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-16 16:48:12 +0300

    fix

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 rem This file is generated from build.pbat, all edits will be lost
-set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\Program Files\7-Zip;C:\Qt\Tools\mingw810_64\bin;C:\Qt\5.15.2\mingw81_64\bin;C:\mysql-8.0.33-winx64\bin;C:\mysql-8.0.33-winx64\lib;%PATH%
+set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\Program Files\7-Zip;C:\Qt\Tools\mingw810_64\bin;C:\Qt\5.15.2\mingw81_64\bin;C:\mysql-8.2.0-winx64\bin;C:\mysql-8.2.0-winx64\lib;%PATH%
 if exist "C:\Program Files\Git\mingw32\bin\curl.exe" set CURL=C:\Program Files\Git\mingw32\bin\curl.exe
 if exist "C:\Program Files\Git\mingw64\bin\curl.exe" set CURL=C:\Program Files\Git\mingw64\bin\curl.exe
 if exist "C:\Windows\System32\curl.exe" set CURL=C:\Windows\System32\curl.exe
@@ -8,7 +8,7 @@ if not defined CURL (
 echo CURL not found
 exit /b
 )
-pip install aqtinstall
+pip install aqtinstall mugicli
 aqt install-qt windows desktop 5.15.2 win64_mingw81 -O C:\Qt
 aqt install-tool windows desktop tools_qtcreator qt.tools.qtcreator -O C:\Qt
 aqt install-tool windows desktop tools_mingw qt.tools.win64_mingw810 -O C:\Qt
@@ -18,17 +18,9 @@ pushd %~dp0
     7z x -y -oC:\ mysql-8.2.0-winx64.zip
 popd
 :mysql820_end
+where libmysql.dll
 if not exist qtbase-everywhere-src-5.15.2.zip "%CURL%" -L -o qtbase-everywhere-src-5.15.2.zip https://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/qtbase-everywhere-src-5.15.2.zip
 if not exist qtbase-everywhere-src-5.15.2 7z x -y qtbase-everywhere-src-5.15.2.zip
 pushd qtbase-everywhere-src-5.15.2\src\plugins
     qmake -- MYSQL_INCDIR="C:/mysql-8.2.0-winx64/include" MYSQL_LIBDIR="C:/mysql-8.2.0-winx64/lib"
-    mingw32-make -j4
-    mingw32-make install
-    copy /y C:\mysql-8.0.33-winx64\bin\libmysql.dll C:\Qt\5.15.2\mingw81_64\bin
-    copy /y C:\mysql-8.0.33-winx64\bin\libssl-1_1-x64.dll C:\Qt\5.15.2\mingw81_64\bin
-    copy /y C:\mysql-8.0.33-winx64\bin\libcrypto-1_1-x64.dll C:\Qt\5.15.2\mingw81_64\bin
 popd
-pyfind C:\Qt > listing.txt
-7z a -y Qt-5.15.2.zip C:\Qt\5.15.2
-7z a -y QtCreator.zip C:\qt\Tools\QtCreator
-7z a -y mingw810_64.zip C:\qt\Tools\mingw810_64
