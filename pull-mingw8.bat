b3a662d7ed18dc7b235a26438dbfbdbda049fb6d
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-16 23:02:43 +0300

    mingw810_64

---
+++ pull-mingw8.bat
@@ -0,0 +1,6 @@
+@echo off
+rem This file is generated from pull-mingw8.pbat, all edits will be lost
+set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\Program Files\7-Zip;%PATH%
+pip install aqtinstall
+aqt install-tool windows desktop tools_mingw qt.tools.win64_mingw810 -O C:\Qt
+7z a -y mingw810_64.zip C:\Qt\Tools\mingw810_64
