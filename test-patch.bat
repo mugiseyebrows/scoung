8b7c4ac7a9805998466f7958684c52a8e9df6568
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-18 17:10:51 +0300

    test

--- test-patch.bat
+++ test-patch.bat
@@ -1,4 +1,4 @@
 @echo off
 rem This file is generated from test-patch.pbat, all edits will be lost
 set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
-where python
+python --version
