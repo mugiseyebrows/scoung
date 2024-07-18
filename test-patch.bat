303e5dbe0835fcc0c98f1457393c0a28b334363f
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-18 17:01:50 +0300

    whats-in-path

---
+++ test-patch.bat
@@ -0,0 +1,4 @@
+@echo off
+rem This file is generated from test-patch.pbat, all edits will be lost
+C:\miniconda3\python.exe whats-in-path.py
+C:\miniconda3\python.exe whats-in-path.py > exes-in-path.txt
