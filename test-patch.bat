bee2d51698eddec7f15fd8af10458cb689f71b55
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-18 17:18:07 +0300

    whats

--- test-patch.bat
+++ test-patch.bat
@@ -1,4 +1,4 @@
 @echo off
 rem This file is generated from test-patch.pbat, all edits will be lost
-C:\miniconda\python.exe whats-in-path.py
-C:\miniconda\python.exe whats-in-path.py > exes-in-path.txt
+C:\miniconda\python.exe whats-in-path.py -e exe > exes-in-path.txt
+C:\miniconda\python.exe whats-in-path.py -e dll > dlls-in-path.txt
