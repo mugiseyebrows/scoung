10ae2a52e9f89c3ccda6c2c9e9518e6d145b86fc
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-21 16:18:06 +0300

    test-msys

--- build-msys.bat
+++ build-msys.bat
@@ -2,6 +2,7 @@
 rem This file is generated from build-msys.pbat, all edits will be lost
 set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%RUNNER_TEMP%\msys64\ucrt64\bin;%RUNNER_TEMP%\msys64\ucrt64\share\qt6\bin;C:\msys64\ucrt64\bin;C:\msys64\ucrt64\share\qt6\bin;%PATH%
 where mugideploy 1> NUL 2> NUL || pip install mugideploy
+:: move(C:\msys64, C:\msys64_, :github)
 qmake6
 mingw32-make -j4
 mugideploy collect --bin release\main.exe --plugins sqldrivers
