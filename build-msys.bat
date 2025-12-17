147589d2efa3719fafd40ec134835f1e9b52f568
Doronin Stanislav <mugisbrows@gmail.com>
2025-12-17 16:11:58 +0300

    wipe

--- build-msys.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem This file is generated from build-msys.pbat, all edits will be lost
-set PATH=C:\msys64\ucrt64\bin;C:\msys64\ucrt64\share\qt6\bin;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
-qmake6
-mingw32-make -j4
-where mugideploy 1> NUL 2> NUL || pip install mugideploy
-mugideploy collect --bin release\main.exe --plugins sqldrivers
--- test-patch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem This file is generated from test-patch.pbat, all edits will be lost
-python whats-in-path.py -e exe > exes-in-path.txt
-python whats-in-path.py -e dll > dlls-in-path.txt
