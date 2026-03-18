805beedd296241dc73cd80ef37956d662100e2eb
Doronin Stanislav <mugisbrows@gmail.com>
2026-03-18 12:34:16 +0300

    msvc build

--- build.bat
+++ build.bat
@@ -1,73 +1,37 @@
 @echo off
 rem This file is generated from build.pbat, all edits will be lost
-set PATH=C:\Windows\System32;C:\Program Files\7-Zip;C:\mingw540_32\bin;C:\Qt\4.8.7p\mingw540_32\bin;C:\mysql-5.5.62-win32\lib;C:\mysql-5.5.62-win32\bin;C:\OpenSSL\bin;C:\Strawberry\perl\bin;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
+set PATH=C:\Windows\System32;C:\Program Files\7-Zip;C:\Qt\4.8.7\msvc\bin;C:\mysql-8.2.0-winx64\lib;C:\mysql-8.2.0-winx64\bin;C:\Strawberry\perl\bin;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%PATH%
 if exist "C:\Program Files\Git\usr\bin\patch.exe" set PATCH=C:\Program Files\Git\usr\bin\patch.exe
 if not defined PATCH (
 echo PATCH not found
 exit /b
 )
-if exist C:\mingw540_32\bin\gcc.exe goto mingw_end
-if not exist i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z curl -L -o i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z "https://netix.dl.sourceforge.net/project/mingw-w64/Toolchains%%20targetting%%20Win32/Personal%%20Builds/mingw-builds/5.4.0/threads-win32/dwarf/i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z"
-7z rn i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z mingw32 mingw540_32
-7z x -y -oC:\ i686-5.4.0-release-win32-dwarf-rt_v5-rev0.7z
-:mingw_end
-if exist C:\mysql-5.5.62-win32\bin\mysql.exe goto mysql_end
-if not exist mysql-5.5.62-win32.zip curl -L -o mysql-5.5.62-win32.zip https://cdn.mysql.com/Downloads/MySQL-5.5/mysql-5.5.62-win32.zip
-7z x -y -oC:\ mysql-5.5.62-win32.zip
+if exist C:\mysql-8.2.0-winx64\bin\mysql.exe goto mysql_end
+if not exist mysql-8.2.0-winx64.zip (
+    echo downloading mysql-8.2.0-winx64.zip
+    curl -L -o mysql-8.2.0-winx64.zip https://cdn.mysql.com/Downloads/MySQL-8.2/mysql-8.2.0-winx64.zip
+)
+7z x -y -oC:\ mysql-8.2.0-winx64.zip
 :mysql_end
 if exist C:\OpenSSL\bin\openssl.exe goto openssl_end
 if not exist OpenSSL_1_0_1m.zip curl -L -o OpenSSL_1_0_1m.zip https://github.com/mugiseyebrows/drenor/releases/download/OpenSSL_1_0_1m/OpenSSL_1_0_1m.zip
 7z x -y -oC:\ OpenSSL_1_0_1m.zip
 :openssl_end
-move /y "C:\Program Files\OpenSSL" "C:\Program Files\OpenSSL_"
+if exist "C:\Program Files\OpenSSL" move /y "C:\Program Files\OpenSSL" "C:\Program Files\OpenSSL_"
 where mugideploy > NUL 2>&1 || pip install mugideploy
 if not exist qt-everywhere-opensource-src-4.8.7.zip curl -L -o qt-everywhere-opensource-src-4.8.7.zip https://download.qt.io/archive/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.zip
 if not exist qt-everywhere-opensource-src-4.8.7 7z x -y qt-everywhere-opensource-src-4.8.7.zip
-set MYSQL_INCLUDE=C:\mysql-5.5.62-win32\include
-set MYSQL_BIN=C:\mysql-5.5.62-win32\lib
 set OPENSSL_BIN=C:\OpenSSL\bin
 set OPENSSL_INCLUDE=C:\OpenSSL\include
 set OPENSSL_MODE=-openssl-linked 
 set OPENSSL_LIBS="-lssleay32 -llibeay32"
-set exclude=-no-declarative -no-multimedia -no-xmlpatterns -no-webkit -no-scripttools -no-script -nomake tests -nomake examples -nomake demos -nomake tools
+set exclude=-no-declarative -no-multimedia  -no-accessibility -no-qt3support -no-xmlpatterns -no-webkit -no-scripttools -no-script -no-phonon -nomake tests -nomake examples -nomake demos -nomake tools
 pushd qt-everywhere-opensource-src-4.8.7
     "%PATCH%" -N -p1 -i ..\0001-fix-doc-script.patch
-    "%PATCH%" -N -p1 -i ..\0001-add-mysql-to-LIBS.patch
-    "%PATCH%" -N -p1 -i ..\0001-profile-flag.patch
-    "%PATCH%" -N -p1 -i ..\0001-uic3fix.patch
-    "%PATCH%" -N -p1 -i ..\0001-mysql-driver.patch
-    "%PATCH%" -N -p1 -i ..\0001-pg-flag-in-specs.patch
-    configure -prefix C:\Qt\4.8.7p\mingw540_32 -opensource -developer-build -confirm-license -static -platform win32-g++ -opengl desktop -debug %exclude% -plugin-sql-mysql -plugin-sql-odbc -I %MYSQL_INCLUDE% -L %MYSQL_BIN% -I %OPENSSL_INCLUDE% -L %OPENSSL_BIN% -openssl-linked OPENSSL_LIBS="-lssleay32 -llibeay32"
-    mingw32-make -j4 sub-tools-bootstrap-all-ordered || exit /b
-    mingw32-make -j4 sub-moc-all-ordered || exit /b
-    mingw32-make -j4 sub-rcc-all-ordered || exit /b
-    mingw32-make -j4 sub-uic-all-ordered || exit /b
-    mingw32-make -j4 sub-winmain-all-ordered || exit /b
-    mingw32-make -j4 sub-corelib-all-ordered || exit /b
-    mingw32-make -j4 sub-xml-all-ordered || exit /b
-    mingw32-make -j4 sub-network-all-ordered || exit /b
-    mingw32-make -j4 sub-sql-all-ordered || exit /b
-    mingw32-make -j4 sub-testlib-all-ordered || exit /b
-    mingw32-make -j4 sub-gui-all-ordered || exit /b
-    mingw32-make -j4 sub-idc-all-ordered || exit /b
-    mingw32-make -j4 sub-activeqt-all-ordered || exit /b
-    mingw32-make -j4 sub-svg-all-ordered || exit /b
-    mingw32-make -j4 sub-tools-bootstrap-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-moc-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-rcc-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-uic-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-winmain-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-corelib-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-xml-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-network-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-sql-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-testlib-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-gui-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-idc-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-activeqt-install_subtargets-ordered || exit /b
-    mingw32-make -j4 sub-svg-install_subtargets-ordered || exit /b
-    mingw32-make install_qmake 
-    mingw32-make install_mkspecs
+    "%PATCH%" -N -p1 -i ..\0001-link-libmysql.patch
+    "%PATCH%" -N -p1 -i ..\0001-typedef-my_bool.patch
+    configure -prefix C:\Qt\4.8.7\msvc -opensource -developer-build -confirm-license -shared -platform win32-msvc2015 -opengl desktop -debug %exclude% -plugin-sql-mysql -plugin-sql-odbc -I C:\mysql-8.2.0-winx64\include -L C:\mysql-8.2.0-winx64\lib
+    nmake || exit /b
+    nmake install || exit /b
 popd
-mugideploy copy-dep --bin C:\Qt\4.8.7p\mingw540_32\bin\qmake.exe --dst C:\Qt\4.8.7p\mingw540_32\bin
-7z a -y Qt-4.8.7.7z C:\Qt\4.8.7p\mingw540_32
+7z a -y Qt-4.8.7.7z C:\Qt\4.8.7\msvc
